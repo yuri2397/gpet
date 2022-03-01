19eab76169b5bd80e6db11ee305f3422058a9dcd
yuri2397 <diawlemor97@gmail.com>
2022-03-01 13:31:19 +0000

    gpet

--- api/resources/views/layouts/app.blade.php
+++ api/resources/views/layouts/app.blade.php
@@ -7,13 +7,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="/css/app.css" rel="stylesheet">
     <title>EDT SERVICE WEB</title>
-
+    
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,600;1,800;1,900&display=swap');
 
         * {
             font-family: "Poppins";
+        }
+
+        .border-raduis{
+            border-raduis: 10px
         }
 
         .center {
