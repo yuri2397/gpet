06276ef28e720fe364ac7eb090f0121c4a1fcf2d
Ablayekandji <kandji.djibrilabdoulaye@gmail.com>
2022-03-12 22:31:28 +0000

    route api and crud function

--- api/app/Http/Controllers/RessourceController.php
+++ api/app/Http/Controllers/RessourceController.php
@@ -7,4 +7,49 @@ use Illuminate\Http\Request;
 class RessourceController extends Controller
 {
     //
+     
+    public function index()
+    {
+        //
+    }
+
+    
+    public function create()
+    {
+        //
+    }
+
+   
+    public function store()
+    {
+        //
+    }
+
+    
+    public function show($id)
+    {
+        //
+    }
+
+    
+    public function edit($id)
+    {
+        //
+    }
+
+    
+    public function update($id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function search($data)
+    {
+        
+    }
 }
