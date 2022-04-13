5de48fc1795c99f32ef101cd837e2f4a96d97124
cheikhtall <cheikhtall1199@gmail.com>
2022-04-13 13:12:38 +0000

    gpet

--- api/app/Http/Controllers/SyllabusController.php
+++ api/app/Http/Controllers/SyllabusController.php
@@ -2,42 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Syllabus;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SyllabusController extends Controller
 {
-    //
-     
+    public function __construct() {
+        $this->middleware("permission:creer syllabus")->only(["store"]);
+    }
+
     public function index()
     {
-        //
+        $user = User::find(auth()->id());
+        if($user->isAdmin()) {
+            return Syllabus::with(['course'])->orderBy('created_at','description')->get();
+        }else{
+            return Syllabus::with(['course'])->whereCourseId($user->course_id)->orderBy('created_at');
+        }
     }
 
-    
-    public function create(Request $request)
-    {
-        //
-    }
-
-   
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'description' => 'required|text',
+            "course_id" => 'required|exists:course,id'
+        ]);
+        $syllabus= new Syllabus();
+        $syllabus->description = $request->description ?? null;
+        $syllabus->course_id = $request->course_id;
+        $syllabus->save();
+        return $syllabus;
     }
 
-    
+
     public function show($id)
     {
         //
     }
 
-    
+
     public function edit($id)
     {
         //
     }
 
-    
+
     public function update(Request $request,$id)
     {
         //
@@ -50,6 +61,6 @@ class SyllabusController extends Controller
 
     public function search($data)
     {
-        
+        //
     }
 }
