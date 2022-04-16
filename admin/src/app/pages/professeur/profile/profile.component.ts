d123068d6b663d290837d16c8d1c64afbf7085aa
Ablayekandji <kandji.djibrilabdoulaye@gmail.com>
2022-04-16 16:12:36 +0000

    partie securiter prof done and partie profile du prof sa reste laffichage qui bug

--- admin/src/app/pages/professeur/profile/profile.component.ts
+++ admin/src/app/pages/professeur/profile/profile.component.ts
@@ -1,6 +1,12 @@
 import { UserService } from 'src/app/services/user.service';
 import { ProfessorService } from 'src/app/services/professor.service';
 import { Component, OnInit } from '@angular/core';
+import { Professor } from 'src/app/models/professor';
+import { Location } from '@angular/common';
+import { User } from 'src/app/models/user';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { NotificationService } from 'src/app/services/notification.service';
+import { ProfesseurEditComponent } from '../professeur-edit/professeur-edit.component';
 
 @Component({
   selector: 'app-profile',
@@ -9,7 +15,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
   isLoad = false;
-  constructor(private userService: UserService) {}
+  professeur:any;
+  dataLoad = true;
+  errorServer = false;
+
+  constructor(private userService: UserService,private notification: NotificationService,
+    private modalService: NzModalService,private profService: ProfessorService,private location: Location,
+    ) {}
 
   ngOnInit(): void {
     this.profile();
@@ -17,12 +29,49 @@ export class ProfileComponent implements OnInit {
 
   profile() {
     this.isLoad = true;
+
     this.userService.currentUser().subscribe({
       next: (response) => {
-        console.log(response);
+        this.professeur=response.professor;
+        console.log(response.professor);
+        console.log(this.professeur);
+        console.log("prof bi "+this.professeur.first_name);
         this.isLoad = false;
       },
-      error: (errors) => {},
+      error: (errors) => {
+
+      },
     });
+  }
+
+
+  openEditModal() {
+    const modal = this.modalService.create({
+      nzTitle: 'Modifier les information',
+      nzContent: ProfesseurEditComponent,
+      nzComponentParams: {
+        professor: this.profService.clone(this.professeur),
+      },
+      nzCentered: true,
+      nzMaskClosable: false,
+      nzClosable: false,
+      nzWidth: '60em',
+    });
+
+    modal.afterClose.subscribe((data: Professor | null) => {
+      if (data != null) {
+        this.professeur = data;
+      }
+    });
+  }
+
+  userProfilePath() {
+    if (this.professeur.avatar == null) {
+      return '/assets/img/avatar.png';
+    }
+    return this.profService.host + this.professeur.avatar;
+  }
+  onBack() {
+    this.location.back();
   }
 }
