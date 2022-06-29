306fe4b0e4eeed827299ba517e16c6c40b2bdfb6
yuri2397 <diawlemor97@gmail.com>
2022-06-29 12:32:55 +0000

    Oaky

--- admin/src/environments/environment.prod.ts
+++ admin/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   host: 'https://api-edtset.uidt.sn/',
-  api: 'https://api-edtset.uidt.sn/api/'
+  api: 'https://api-edtset.uidt.sn/api/',
+  version: "1.0.0"
 };
