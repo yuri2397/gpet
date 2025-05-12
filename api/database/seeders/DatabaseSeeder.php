ebf3fbebffa661e146023ed1508ad5af929552ed
mordiaw <diaw@tanelhealth.com>
2025-05-12 20:25:41 +0000

    MIGRATION VERS LARAVEL 10

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            CourseStatus::class,
-            GenDays::class,
-            GenRoles::class,
-            GenService::class,
-        ]);
+        // $this->call([
+        //     CourseStatus::class,
+        //     GenDays::class,
+        //     GenRoles::class,
+        //     GenService::class,
+        // ]);
+        // new user
+        $user = new User();
+        $user->first_name = "Mor";
+        $user->last_name = "Diaw";
+        $user->email = "admin@gmail.com";
+        $user->avatar = "https://ui-avatars.com/api/?name=Mor+Diaw";
+        $user->email_verified_at = now();
+        $user->password = Hash::make("Matrix@2397!");
+        $user->save();
     }
 }
